18df0036d40f92554402a0e9f3b600102bd84fcd
Quang Huy <huygood10@example.com>
2025-11-23 11:00:46 +0700

    fix: update base path in vite.config.js to match project structure

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/TODO/",
+  base: "/ToDolist_REACT/",
   plugins: [react(), tailwindcss()],
   server: {
     port: 3000,

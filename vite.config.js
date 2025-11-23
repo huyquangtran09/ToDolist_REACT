11043c03f3c2424d49788ba5e131e40e01833292
Quang Huy <huygood10@example.com>
2025-11-23 10:52:42 +0700

    fix: update base path in vite.config.js to match naming convention

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/TodoList/",
+  base: "/TODO/",
   plugins: [react(), tailwindcss()],
   server: {
     port: 3000,
